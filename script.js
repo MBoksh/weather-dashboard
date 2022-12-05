2275efce24d92e659c68d877bcdbd56fffc40e74
MBoksh <mikaaeelboksh@hotmail.com>
2022-12-05 18:07:33 +0000

    added javascript to select dom objects and add events

---
+++ script.js
@@ -0,0 +1,12 @@
+// Searched input text
+let searchedLocationElement = document.getElementById('location');
+// Button to submit
+let findWeatherButtonElement = document.getElementById('search-btn');
+
+// Function to retrieve weather from api when city searched for
+function getWeatherLocation() {
+
+};
+
+// Event listener for findWeatherButtonElement
+findWeatherButtonElement.addEventListener('click', getWeatherLocation)
